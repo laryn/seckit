b808a09b8ac6fdee2d5e31fd23c9866f45020b46
jweowu <ps@152788.no-reply.drupal.org>
2022-02-05 16:55:16 +1300

    Document the previous change

--- seckit.api.php
+++ seckit.api.php
@@ -19,7 +19,10 @@
  * @see _seckit_get_options().
  */
 function hook_seckit_options_alter(&$options) {
+  // Set the X-Frame-Options HTTP header value to SAMEORIGIN.
   $options['seckit_clickjacking']['x_frame'] = SECKIT_X_FRAME_SAMEORIGIN;
+  // Add a new CSP directive "foo-src example.com".
+  $options['seckit_xss']['csp']['foo-src'] = 'example.com';
 }
 
 /**
